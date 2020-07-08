df2ad647d84881eea97b4870a8582fb2decf6aee
learner1909 <ritikj019@gmail.com>
2020-07-08 12:44:16 +0530

    08-07-2020
    New Changes

--- js/index.js
+++ js/index.js
@@ -1,22 +1,14 @@
-
-
 const listitems = document.querySelectorAll(".list-item");
-const media = window.matchMedia("(min-width:1000px)");
+const media = window.matchMedia("(min-width:800px)");
 const navbtn = document.getElementById("navbtn");
+const navlist = document.getElementById("navlist");
 
 
 function togglenav() {
     listitems.forEach(item => item.classList.toggle("hide"));
-}
-
-function myfunc() {
-    if (media.matches) {
-        navbtn.classList.add("hide");
-        listitems[0].classList.remove("mt-2");
+    if (navlist.style.right == "0%") {
+        navlist.style.right = "-30%";
     } else {
-        navbtn.classList.remove("hide");
-        listitems[0].classList.add("mt-2");
+        navlist.style.right = "0%";
     }
 }
-
-media.addListener(myfunc);
