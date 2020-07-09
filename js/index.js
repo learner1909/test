e30fa64d5e4e262c7b00ed622783189fb592957b
learner1909 <ritikj019@gmail.com>
2020-07-09 15:52:56 +0530

    Finally NAVBAR DONE

--- js/index.js
+++ js/index.js
@@ -8,9 +8,7 @@ function togglenav() {
     listitems.forEach(item => item.classList.toggle("hide"));
     if (navlist.style.right == "0%") {
         navlist.style.right = "-100%";
-        document.getElementsByTagName("BODY")[0].style.overflow="auto";
     } else {
         navlist.style.right = "0%";
-        document.getElementsByTagName("BODY")[0].style.overflow="hidden";
     }
 }
