89ea9b21ac9067c06655d0dbb36e0c1dde725077
learner1909 <ritikj019@gmail.com>
2020-07-09 15:38:47 +0530

    NavBar Final Fixed

--- js/index.js
+++ js/index.js
@@ -8,7 +8,9 @@ function togglenav() {
     listitems.forEach(item => item.classList.toggle("hide"));
     if (navlist.style.right == "0%") {
         navlist.style.right = "-100%";
+        document.getElementsByTagName("BODY")[0].style.overflow="auto";
     } else {
         navlist.style.right = "0%";
+        document.getElementsByTagName("BODY")[0].style.overflow="hidden";
     }
 }
