001bddf07c94b2659e445792acbbbb06e75c483e
Ritik Jaiswal <68381427+ritikjaiswal019@users.noreply.github.com>
2020-10-04 15:28:06 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,8 +7,12 @@ const navlist = document.getElementById("navlist");
 function togglenav() {
     listitems.forEach(item => item.classList.toggle("hide"));
     if (navlist.style.right == "0%") {
-        navlist.style.right = "-100%";
-    } else {
+        navlist.style.right = "-120%";
+    }
+    else if(navlist.style.right == "50%")
+        navlist.style.right = "-120%";
+    else {
         navlist.style.right = "0%";
     }
+    
 }
